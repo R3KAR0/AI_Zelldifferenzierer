9a31b715ae5381e2399e90667d824a394ffca5b8
Marthere <45464832+R3KAR0@users.noreply.github.com>
2022-06-28 22:08:28 +0200

    Refactoring

--- UserService/Consumers/RegisterRequestConsumer.cs
+++ UserService/Consumers/RegisterRequestConsumer.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using UserServiceModels;
 using UserServiceModels.Exceptions;
 using UserServiceRequestMessages;
+using UserServiceResponseMessages;
 
 namespace UserService.Consumers
 {
